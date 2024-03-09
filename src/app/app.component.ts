526f90efa7eb4b6205f04534fee1b01227ec4a64
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-09 17:40:02 +0530

    *ngIf and *ngFor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives-deep-dive';
+  numbers = [1, 2, 3, 4, 5]
+  oddNumbers = [1, 3, 5]
+  evenNumbers = [2, 4]
+  onlyOdd = false;
 }
