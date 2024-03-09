0f991b71d69101c6b24e45985c47ca6ee05c08d7
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-09 17:54:21 +0530

    Remove server ssl self-signed certificates

---
+++ src/app/basic-highlight/basic-highligt.directive.ts
@@ -0,0 +1,8 @@
+import { Directive, ElementRef } from "@angular/core";
+
+@Directive({
+selector: '[appBasicHighlight]'
+})
+export class BasicHighlightDirective {
+    constructor(private elementRef: ElementRef) {}
+}
