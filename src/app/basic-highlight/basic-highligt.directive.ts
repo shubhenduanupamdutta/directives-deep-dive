04572ac1e8619a6269520b0de164cbde241c7ec4
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-09 18:05:06 +0530

    Create a basic attribute directive
    Created a appBasicHighlight custom directive which turns background green

--- src/app/basic-highlight/basic-highligt.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ElementRef } from "@angular/core";
-
-@Directive({
-selector: '[appBasicHighlight]'
-})
-export class BasicHighlightDirective {
-    constructor(private elementRef: ElementRef) {}
-}
